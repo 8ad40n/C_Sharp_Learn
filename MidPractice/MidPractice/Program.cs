52b6596a324c5d3731cfe4bc563df72c94b51a96
Badhon Nath Joy <8ad40n@gmail.com>
2023-03-05 22:05:40 +0600

    added

--- MidPractice/MidPractice/Program.cs
+++ MidPractice/MidPractice/Program.cs
@@ -27,10 +27,37 @@ namespace MidPractice
             Console.WriteLine();
             Console.WriteLine();
 
-            //CLASS: 
+            //CLASS: Enumeration
             Console.WriteLine("CLASS: Enumeration:");
             Enumeration e = new Enumeration();
             e.PrintEnum();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //TYPE CASTING:
+
+            //Explicit Casting (manually) - converting a larger type to a smaller size type:
+            //information might be lost:
+            double myDouble = 9.78;
+            int myInt = (int)myDouble;   //double to int
+
+            Console.WriteLine("Explicit:");
+            Console.WriteLine(myDouble);   // Outputs 9.78
+            Console.WriteLine(myInt);      // Outputs 9
+
+            //Implicit Casting (automatically) - converting a smaller type to a larger type size:
+            int yInt = 9;
+            double yDouble = myInt;       // Automatic casting: int to double
+
+            Console.WriteLine("Implicit:");
+            Console.WriteLine(yInt);      // Outputs 9
+            Console.WriteLine(yDouble);   // Outputs 9
+
+
+
+
 
         }
     }
