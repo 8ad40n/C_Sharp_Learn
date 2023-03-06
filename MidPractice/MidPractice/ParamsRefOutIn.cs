b85e8072673d4ebbeecf4c00f52b7124c202928a
Badhon Nath Joy <8ad40n@gmail.com>
2023-03-06 16:18:26 +0600

    added

---
+++ MidPractice/MidPractice/ParamsRefOutIn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidPractice
+{
+    internal class ParamsRefOutIn
+    {
+        //params:
+        public void Summation(params int[] arr)
+        {
+            int sum = 0;
+            byte c = 0;
+            while (c < arr.Length)
+            {
+                sum += arr[c];
+                c++;
+            }
+            Console.WriteLine("{0}", sum);
+        }
+
+        //ref:
+
+        public void Swap(ref int a,ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+            Console.WriteLine("a:{0},b:{1}", a, b);
+        }
+
+        //out:
+
+        public void Method(out int answer, out string message, out string stillNull)
+        {
+            answer = 44;
+            message = "I've been returned";
+            stillNull = null;
+        }
+
+
+
+
+
+    }
+}
--- MidPractice/MidPractice/Program.cs
+++ MidPractice/MidPractice/Program.cs
@@ -125,7 +125,7 @@ namespace MidPractice
             Console.WriteLine();
             Console.WriteLine();
 
-            //OBJECT:
+            //OBJECT:   //
 
             Console.WriteLine("OBJECT:");
 
@@ -138,6 +138,77 @@ namespace MidPractice
             Console.WriteLine(o2.GetType());
             Console.WriteLine(o3.GetType());
             Console.WriteLine(o4.GetType());
+
+            Console.WriteLine();
+
+            var n1 = 12;
+            var n2 = 3.42;
+            var n3 = "bye";
+            var n4 = new Parent();
+            Console.WriteLine(n1.GetType());
+            Console.WriteLine(n2.GetType());
+            Console.WriteLine(n3.GetType());
+            Console.WriteLine(n4.GetType());
+
+
+
+            //DYNAMIC:
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Dynamic:");
+            dynamic v1 = 23;
+            dynamic v2 = new Parent();
+            //v2.FakeMethod();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //PARAMS: //
+
+            Console.WriteLine("PARAMS:");
+            
+            int[] a1 = new int[5] { 3, 5, 8, 2, 7 };
+            int[] a2 = new int[7] { 9, 4, 3, 6, 9, 5, 1 };
+            ParamsRefOutIn s = new ParamsRefOutIn();
+            s.Summation(3, 4, 7);
+            s.Summation(1, 9);
+            s.Summation(3, 5, 8, 4, 8, 1, 3, 2);
+            s.Summation();
+            s.Summation(5, 3, 8, 2);
+            s.Summation(a1);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //REF: //
+
+            Console.WriteLine("REF:");
+
+            int x1 = 10, y1 = 20;
+            s.Swap(ref x1,ref y1);
+            Console.WriteLine("x1:{0},y1:{1}", x1, y1);   // without ref => x1= 10 and y1= 20
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //OUT:   //
+            Console.WriteLine("OUT:");
+
+
+            int argNumber;
+            string argMessage, argDefault;
+            s.Method(out argNumber, out argMessage, out argDefault);
+            Console.WriteLine(argNumber);
+            Console.WriteLine(argMessage);
+            Console.WriteLine(argDefault);
+
 
             Console.ReadLine();
 
